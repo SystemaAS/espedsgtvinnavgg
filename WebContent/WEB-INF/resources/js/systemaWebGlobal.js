003a1ab731cc0dda311a4aa98f6af20dbf2e0f3c
Oscar de la Torre <oscar@systema.no>
2018-11-10 15:07:44 +0100

    Update

--- WebContent/WEB-INF/resources/js/systemaWebGlobal.js
+++ WebContent/WEB-INF/resources/js/systemaWebGlobal.js
@@ -1,7 +1,7 @@
 	var langMap = {
 			   'EN' : 'English',
 			   'DK' : 'Danish',
-			   'SE' : 'Swedish',
+			   'SV' : 'Swedish',
 			   'NO' : 'Norwegian-Bokmal'
 	}
 	//for setting user lang to datatables
